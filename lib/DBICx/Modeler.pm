47a66272fdf607c568420d80e75249c813b94474
robertkrimen <robertkrimen@gmail.com>
2009-04-21 13:02:54 -0700

    Initial import

---
+++ lib/DBICx/Modeler.pm
@@ -0,0 +1,107 @@
+package DBICx::Modeler;
+
+use warnings;
+use strict;
+
+=head1 NAME
+
+DBICx::Modeler - The great new DBICx::Modeler!
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use DBICx::Modeler;
+
+    my $foo = DBICx::Modeler->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 FUNCTIONS
+
+=head2 function1
+
+=cut
+
+sub function1 {
+}
+
+=head2 function2
+
+=cut
+
+sub function2 {
+}
+
+=head1 AUTHOR
+
+Robert Krimen, C<< <rkrimen at cpan.org> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-dbicx-modeler at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=DBICx-Modeler>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc DBICx::Modeler
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=DBICx-Modeler>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/DBICx-Modeler>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/DBICx-Modeler>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/DBICx-Modeler/>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2009 Robert Krimen, all rights reserved.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+
+=cut
+
+1; # End of DBICx::Modeler
