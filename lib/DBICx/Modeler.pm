5c4322c8040f7a8a54ba392135d0882f2d5da5fc
robertkrimen <robertkrimen@gmail.com>
2009-06-12 09:11:45 -0700

    Release of 0.004

--- lib/DBICx/Modeler.pm
+++ lib/DBICx/Modeler.pm
@@ -9,11 +9,11 @@ DBICx::Modeler - A Moose-based model layer over DBIx::Class
 
 =head1 VERSION
 
-Version 0.003
+Version 0.004
 
 =cut
 
-our $VERSION = '0.003';
+our $VERSION = '0.004';
 
 =head1 SYNOPSIS
 
